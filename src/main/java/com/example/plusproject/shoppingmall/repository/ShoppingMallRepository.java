fb757ac7344584df0e2aacfb97a6944dd7582a6d
전영환 <jeon-younghwan@jeon-yeonghwan-ui-MacBookPro.local>
2025-03-27 19:47:31 +0900

    repository 수정

--- src/main/java/com/example/plusproject/shoppingmall/repository/ShoppingMallRepository.java
+++ src/main/java/com/example/plusproject/shoppingmall/repository/ShoppingMallRepository.java
@@ -3,6 +3,8 @@ package com.example.plusproject.shoppingmall.repository;
 import com.example.plusproject.shoppingmall.entity.ShoppingMall;
 import com.example.plusproject.shoppingmall.enums.StoreStatus;
 import com.example.plusproject.shoppingmall.enums.TotalRating;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
